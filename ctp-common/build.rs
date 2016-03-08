7614cd26bb3f457ce39de1f4311102166f780c3a
Wangshan Lu <wisagan@gmail.com>
2016-03-08 23:50:37 +0800

    (common) remove so linking

--- ctp-common/build.rs
+++ ctp-common/build.rs
@@ -4,16 +4,8 @@ use std::io::Write;
 
 fn main() {
     let out_dir = std::env::var("OUT_DIR").unwrap();
-    let current_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let binding = bindgen::builder().header("../api-ctp/include/ThostFtdcUserApiStruct.h").generate().expect("failed to generate binding");
-    let out_path = format!("{}/ctp-api.rs.in", out_dir);
     let binding_output = binding.to_string().replace("c_char", "c_uchar");
+    let out_path = format!("{}/ctp-api.rs.in", out_dir);
     std::fs::File::create(out_path).expect("can't create file for binding output").write_all(binding_output.as_bytes()).expect("can't write binding output to file");
-    let so_symlink_string = format!("{}/libthostmduserapi.so", out_dir);
-    let so_symlink = std::path::Path::new(&so_symlink_string);
-    if so_symlink.exists() {
-        std::fs::remove_file(so_symlink).expect("symlink exists, but failed to remove it");
-    }
-    std::os::unix::fs::symlink(&format!("{}/../api-ctp/lib/thostmduserapi.so", current_dir), so_symlink).expect("failed to create new symlink");
-    println!("cargo:rustc-link-search=native={}", out_dir);
 }

d03361e94ec4e963144fb56b93afe03fac0a6dce
Wangshan Lu <wisagan@gmail.com>
2016-03-08 20:19:28 +0800

    Implement ctp-common

--- ctp-common/src/lib.rs
+++ ctp-common/src/lib.rs
@@ -1,6 +1,52 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+mod binding {
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+include!(concat!(env!("OUT_DIR"), "/ctp-api.rs.in"));
+}
+
+use std::os::raw::c_int;
+
+pub use binding::*;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum DisconnectionReason {
+    ReadError = 0x1001,
+    WriteError = 0x1002,
+    HeartbeatTimeout = 0x2001,
+    HeartbeatSendError = 0x2002,
+    ErrorMessageReceived = 0x2003,
+    Unknown = 0x0000,
+}
+
+pub fn from_reason_to_disconnection_reason(reason: c_int) -> DisconnectionReason {
+    match reason {
+        0x1001 => DisconnectionReason::ReadError,
+        0x1002 => DisconnectionReason::WriteError,
+        0x2001 => DisconnectionReason::HeartbeatTimeout,
+        0x2002 => DisconnectionReason::HeartbeatSendError,
+        0x2003 => DisconnectionReason::ErrorMessageReceived,
+        _ => DisconnectionReason::Unknown,
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum ApiError {
+    NetworkError = -1,
+    QueueFull = -2,
+    Throttled = -3,
+    Unknown = -4,
+}
+
+#[must_use]
+pub type ApiResult = Result<(), ApiError>;
+
+pub fn from_api_return_to_api_result(api_return: c_int) -> ApiResult {
+    match api_return {
+        0 => Ok(()),
+        -1 => Err(ApiError::NetworkError),
+        -2 => Err(ApiError::QueueFull),
+        -3 => Err(ApiError::Throttled),
+        _ => Err(ApiError::Unknown),
     }
 }

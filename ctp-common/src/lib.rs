a04e0bbdc2a67305cf60d2fd91179eab41aa4c1e
Wangshan Lu <wisagan@gmail.com>
2016-03-25 18:52:24 +0800

    (common) add exchange timestamp conversion

--- ctp-common/src/lib.rs
+++ ctp-common/src/lib.rs
@@ -1,13 +1,17 @@
 #![feature(ptr_as_ref)]
 extern crate encoding;
+extern crate simple_error;
+extern crate time;
 
 mod binding;
 
 use encoding::{ DecoderTrap, Encoding };
 use encoding::all::GB18030;
+use simple_error::SimpleError;
 use std::ascii::AsciiExt;
 use std::borrow::Cow;
 use std::os::raw::c_int;
+use time::{ Timespec, Tm };
 
 pub use binding::*;
 
@@ -119,10 +123,108 @@ pub fn from_rsp_info_to_rsp_result(rsp_info: *const Struct_CThostFtdcRspInfoFiel
     }
 }
 
+pub fn get_exchange_timestamp(md: &Struct_CThostFtdcDepthMarketDataField) -> Result<Timespec, SimpleError> {
+    let year = match ::std::str::from_utf8(&md.TradingDay[0..4]) {
+        Ok(year_str) => {
+            match year_str.parse::<u16>() {
+                Ok(year) => year,
+                Err(err) => {
+                    return Err(SimpleError::new(format!("invalid year string, {}", err)));
+                },
+            }
+        },
+        Err(err) => {
+            return Err(SimpleError::new(format!("year not utf8, {}", err)));
+        },
+    };
+    let month = match ::std::str::from_utf8(&md.TradingDay[4..6]) {
+        Ok(month_str) => {
+            match month_str.parse::<u8>() {
+                Ok(month) => month,
+                Err(err) => {
+                    return Err(SimpleError::new(format!("invalid month string, {}", err)));
+                },
+            }
+        },
+        Err(err) => {
+            return Err(SimpleError::new(format!("month not utf8, {}", err)));
+        },
+    };
+    let day = match ::std::str::from_utf8(&md.TradingDay[6..8]) {
+        Ok(day_str) => {
+            match day_str.parse::<u8>() {
+                Ok(day) => day,
+                Err(err) => {
+                    return Err(SimpleError::new(format!("invalid day string, {}", err)));
+                },
+            }
+        },
+        Err(err) => {
+            return Err(SimpleError::new(format!("day not utf8, {}", err)));
+        },
+    };
+    let hour = match ::std::str::from_utf8(&md.UpdateTime[0..2]) {
+        Ok(hour_str) => {
+            match hour_str.parse::<u8>() {
+                Ok(hour) => hour,
+                Err(err) => {
+                    return Err(SimpleError::new(format!("invalid hour string, {}", err)));
+                },
+            }
+        },
+        Err(err) => {
+            return Err(SimpleError::new(format!("hour not utf8, {}", err)));
+        },
+    };
+    let minute = match ::std::str::from_utf8(&md.UpdateTime[3..5]) {
+        Ok(minute_str) => {
+            match minute_str.parse::<u8>() {
+                Ok(minute) => minute,
+                Err(err) => {
+                    return Err(SimpleError::new(format!("invalid minute string, {}", err)));
+                },
+            }
+        },
+        Err(err) => {
+            return Err(SimpleError::new(format!("minute not utf8, {}", err)));
+        },
+    };
+    let second = match ::std::str::from_utf8(&md.UpdateTime[6..8]) {
+        Ok(second_str) => {
+            match second_str.parse::<u8>() {
+                Ok(second) => second,
+                Err(err) => {
+                    return Err(SimpleError::new(format!("invalid second string, {}", err)));
+                },
+            }
+        },
+        Err(err) => {
+            return Err(SimpleError::new(format!("second not utf8, {}", err)));
+        },
+    };
+    let nanosec = md.UpdateMillisec as i32 * 1000 * 1000;
+    let tm = Tm { tm_sec: second as i32,
+                  tm_min: minute as i32,
+                  tm_hour: hour as i32,
+                  tm_mday: day as i32,
+                  tm_mon: month as i32 - 1,
+                  tm_year: year as i32 - 1900,
+                  tm_wday: 0i32,
+                  tm_yday: 0i32,
+                  tm_isdst: 0i32,
+                  tm_utcoff: 8i32 * 60 * 60, // UTC+8
+                  tm_nsec: nanosec };
+    Ok(tm.to_timespec())
+
+}
+
 #[cfg(test)]
 mod tests {
     use std::borrow::Cow;
+    use time::Timespec;
     use super::gb18030_cstr_to_str;
+    use super::get_exchange_timestamp;
+    use super::Struct_CThostFtdcDepthMarketDataField;
 
     #[test]
     fn cstr_conversion_empty_str() {
@@ -160,5 +262,15 @@ mod tests {
     #[test]
     fn cstr_conversion_gb2312_cstr() {
         assert_eq!(gb18030_cstr_to_str(b"\xd5\xfd\xc8\xb7\0"), "正确");
+    }
+
+    #[test]
+    fn exchange_timestamp_conversion() {
+        let mut md: Struct_CThostFtdcDepthMarketDataField = Default::default();
+        md.TradingDay = *b"19700101\0";
+        md.UpdateTime = *b"08:00:00\0";
+        md.UpdateMillisec = 0;
+        let ts = get_exchange_timestamp(&md);
+        assert_eq!(Ok(Timespec{ sec: 0, nsec: 0 }), ts);
     }
 }

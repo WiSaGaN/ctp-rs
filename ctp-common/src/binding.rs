14637e67317a23c75bb6055b203a8ea2c4799470
Wangshan Lu <wisagan@gmail.com>
2016-04-27 23:46:11 +0800

    (common) add bool type consts

--- ctp-common/src/binding.rs
+++ ctp-common/src/binding.rs
@@ -4,7 +4,11 @@
 include!(concat!(env!("OUT_DIR"), "/struct.rs.in"));
 include!(concat!(env!("OUT_DIR"), "/data_type.rs.in"));
 
+pub const THOST_FTDC_BOOL_TRUE: c_int = 1;
+pub const THOST_FTDC_BOOL_FALSE: c_int = 0;
+
 use std::fmt;
+use std::os::raw::c_int;
 use super::{ gb18030_cstr_to_str, normalize_double };
 
 impl fmt::Debug for Struct_CThostFtdcRspAuthenticateField {

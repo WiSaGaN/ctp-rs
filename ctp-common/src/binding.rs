5d8882bd1ada94b77256e639c1500fae3caefe26
Wangshan Lu <wisagan@gmail.com>
2020-05-12 22:21:21 +0800

    (common) suppress clippy warning

--- ctp-common/src/binding.rs
+++ ctp-common/src/binding.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![allow(clippy::unreadable_literal)]
 include!("generated/struct.rs.in");
 include!("generated/error.rs.in");
 
